cf02e00c9bba2c771a19c75c091d4baeb6a93cd6
Georgy Gerbst <gerbst.g@gmail.com>
2019-05-22 23:50:04 +0300

    initial + static

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.scss';
+
+const App = () => {
+	return (
+		<main role="main" className="container">
+1213132
+		</main>
+	);
+};
+
+export default App;
